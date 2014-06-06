4bd291cb10dbf29d166e2d2b7b72a9683b3d8949
Jeremy Michael Crosbie <jcrosbie@bertramcapital.com>
2014-06-06 13:57:03 -0600

    Only shows apps on dashboard if user has the role.
    Limit the visibility of applications by role.

--- grails-app/controllers/spud/core/admin/DashboardController.groovy
+++ grails-app/controllers/spud/core/admin/DashboardController.groovy
@@ -5,9 +5,10 @@ import  spud.core.*
 @SpudSecure(['AUTHORIZED'])
 class DashboardController {
 	static namespace = 'spud_admin'
+	def adminApplicationService
 
     def index() {
-    	def adminApplications = grailsApplication.config.spud.core.adminApplications
+    	def adminApplications = adminApplicationService.myApplications()
     	
     	render view: '/spud/admin/dashboard/index', model: [adminApplications: adminApplications, breadCrumbs:[["Dashboard", "/spud/admin"]]]
     }
